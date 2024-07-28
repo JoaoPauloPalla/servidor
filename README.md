2f1b88f6c4b7aa0981fdfc982f2f0b5cdfbc0849
Joao Paulo Palla <joaopaulopalla@gmail.com>
2024-07-27 21:32:19 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# servidor
-Repositório contendo as configurações de um servidor na Digital Ocean para projetos pessoais
+# Servidor
+
+Repositório contendo as configurações de um servidor na DigitalOcean para projetos pessoais.
+
+## Arquitetura
+
+Servidor composto por 4 partes principais:
+
+1. Infra: banco de dados (postgresql) e broker MQTT
+2. Backend: apis e serviços
+3. Frontend: aplicativos
+4. Backups
+
+
+### Infra: Postgresql
+
+### Infra: MQTT Broker
+
+[Tutorial]( https://techknowsurf.blogspot.com/2022/07/Use-SSL-TLS-with-Mosquitto-Broker.html)
+
+Configuração da conexão TLS/SSL:
+
+
+Integração PostgreSQL-MQTT
+
+https://github.com/iegomez/mosquitto-go-auth
+
+https://medium.com/@minhhieugma/authentication-in-mosquitto-mqtt-with-postgresql-in-docker-7c147e49bad0
+
+https://stackoverflow.com/questions/77929179/mosquitto-go-auth-with-postgres-authorization-error
+
+http://www.steves-internet-guide.com/mossquitto-conf-file/
+
+Script IA
+Codeium, a partir de agora, preciso que você seja um engenheriro/arquiteto de software com 15 anos de experiência, especialista em elaboração de sistesmas de internet das coisas, que me responda como um senior responde uma pessoa sem muita experiência, e que mastigue a maioria das coisas que eu vou te pedir, pois preciso conseguir entender o que vai me falar.
+
+Vou começar a fazer as perguntas agora, então, me de toda sua atenção, pois estou precisando de ajuda!
---
+++ server/infra/postgresql/scripts/init.sql
@@ -0,0 +1,46 @@
+
+--Database Definition
+SET TIMEZONE TO 'America/Sao_Paulo';
+
+-- User Definition
+CREATE USER mqtt WITH PASSWORD 'mqttpassword';
+CREATE USER api WITH PASSWORD 'apipassword';
+CREATE USER front WITH PASSWORD 'frontpassword';
+
+
+-- Table Definition
+CREATE TABLE IF NOT EXISTS "public"."MqttUsers" (
+    "id" serial NOT NULL,
+    "userName" varchar(50) NOT NULL unique,
+    "passwordHash" varchar(255) NOT NULL,
+    "isAdmin" boolean NOT NULL DEFAULT true,
+    "isActive" boolean NOT NULL DEFAULT true,
+    "createdAt" timestamptz DEFAULT NOW(),
+    "updatedAt" timestamptz NULL,
+    PRIMARY KEY ("id"),
+);
+
+CREATE TABLE IF NOT EXISTS "public"."MqttAcls" (
+    "id" serial NOT NULL,
+    "topic" varchar(50),
+    "rw" integer CHECK ("rw" IN (0, 1, 2, 3, 4, 11, 999)) DEFAULT 0,
+    "userId" integer NOT NULL,
+    "createdAt" timestamptz DEFAULT NOW(),
+    "updatedAt" timestamptz NOT NULL ,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("userId") REFERENCES "public"."MqttUsers"("id") ON UPDATE CASCADE ON DELETE CASCADE 
+);
+COMMENT ON COLUMN public."MqttAcls".rw 
+    IS 'Options:  0: None / 1: Read / 2: Write / 3: Read and Write / 4: Subscibe / 11: Deny / 999: all permissions';
+
+-- Permission Definition
+-- GRANT READ ON TABLE "public"."MqttAcls" TO mqtt;
+-- GRANT READ ON TABLE "public"."MqttUsers" TO mqtt; 
+
+--Insert Data
+INSERT INTO "public"."MqttAcls" ( "topic", "rw", "userId") VALUES ( 'topic1', 3, '1');
+
+INSERT INTO "public"."MqttUsers" ( "userName", "passwordHash", "isAdmin", "isActive") VALUES
+( 'user1', '$2b$10$uEyoPdHpXvG.F1JhU7X5fO.6hiZqZNSh/nq4AwPVhR/oG70jRvSmS', 't', 't'),
+( 'admin', '$2b$10$uEyoPdHpXvG.F1JhU7X5fO.6hiZqZNSh/nq4AwPVhR/oG70jRvSmS', 't', 't');
+
